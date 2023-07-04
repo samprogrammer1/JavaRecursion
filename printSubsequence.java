b0f4b1e23325ca0ad44d48c52586e4697ef078c0
unknown <sasumro5@gmail.com>
2023-07-04 10:33:24 +0530

    print subsequence solve question

---
+++ printSubsequence.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class printSubsequence {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+
+        printSS(str , "");
+
+    }
+
+    public static void printSS(String str , String ans){
+        if(str.length() == 0){
+            System.out.println(ans);
+            return;
+        }
+
+        char ch = str.charAt(0);
+        String roq = str.substring(1);
+        
+        printSS(roq, ans+ch);
+        printSS(roq, ans + "");
+
+
+    }
+}
